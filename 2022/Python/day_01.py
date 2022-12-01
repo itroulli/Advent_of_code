b63fc0add7ca69e5abd13f1503b18db1d8f208ad
Ilias Troullinos <htrul18@gmail.com>
2022-12-01 22:39:46 +0100

    add 2022/day_01

---
+++ 2022/Python/day_01.py
@@ -0,0 +1,23 @@
+import os
+from itertools import groupby
+
+PARENT_DIR = os.path.join(os.cwd(), os.pardir)
+INPUT = os.path.join(PARENT_DIR, "inputs", "day_01.txt")
+
+
+def solve_day1(part_two=False):
+    with open(INPUT, encoding="utf-8") as text:
+        input_text = text.readlines()
+
+    elves = [list(y) for x, y in groupby(input_text, lambda z: z == "\n") if not x]
+    elves_inv = [sum(map(int, elf)) for elf in elves]
+    if part_two:
+        elves_inv.sort(reverse=True)
+        print(f"Answer to part 2: {sum(elves_inv[:3])}")
+    else:
+        print(f"Answer to part 1: {max(elves_inv)}")
+
+
+if __name__ == "__main__":
+    solve_day1()
+    solve_day1(part_two=True)
---
+++ 2022/README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2022
+
+Here you can find my solutions for AoC 2022 in Python:
+
+- [Day 1](https://adventofcode.com/2022/day/1) - [Solution](https://github.com/itroulli/Advent_of_code/blob/main/2022/Python/day_01.py)
